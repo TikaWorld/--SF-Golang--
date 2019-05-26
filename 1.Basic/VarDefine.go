5f9da4845e150fd660b2019032e0c45bb6627bc4
TikaWorld <tikaworld0416@gmail.com>
2019-05-26 22:44:03 +0900

    study var define, calculate, Type define and ControlFlow in golang

---
+++ 1.Basic/VarDefine.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int // int, int, int
+	var Bool bool
+	var String string
+	var b, f, s = true, 2.3, "four" // bool, float64, string
+
+	fmt.Println(i, Bool, String)
+	fmt.Println(b, f, s)
+}
