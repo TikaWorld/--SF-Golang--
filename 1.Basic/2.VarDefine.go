1d344a50c1478ee03c7ec56f2b49abe786697995
TikaWorld <tikaworld0416@gmail.com>
2019-05-31 10:09:07 +0900

    Study OOP(Method, Interface), ErrorHandle in golang

--- 1.Basic/2.VarDefine.go
+++ 1.Basic/2.VarDefine.go
@@ -7,7 +7,13 @@ func main() {
 	var Bool bool
 	var String string
 	var b, f, s = true, 2.3, "four" // bool, float64, string
+	const (
+		Int   = 1
+		Flaot = 0.1
+	)
 
 	fmt.Println(i, Bool, String)
 	fmt.Println(b, f, s)
+	fmt.Println(Int, Flaot)
+	fmt.Println()
 }
