70fd844ff342d8f9f3c1a1c9e235242cdddbe7ec
TikaWorld <tikaworld0416@gmail.com>
2019-06-02 22:05:34 +0900

    Study panic, GoRoutine in golang

---
+++ 1.Basic/16.GoRoutine.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	runtime.GOMAXPROCS(11)
+	var wait sync.WaitGroup
+	wait.Add(2)
+
+	go func() {
+		defer wait.Done()
+		fmt.Println("Hello")
+	}()
+
+	go func(msg string) {
+		defer wait.Done()
+		fmt.Println(msg)
+	}("Hi")
+
+	wait.Wait()
+}
